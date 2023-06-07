c4aaa7cde7ec4104062f34587bcf468b780690f4
filipenkaProg <Maksim.Filipenka@itechart-group.com>
2023-06-07 17:40:51 +0200

    initial commit

--- app/domain/employee.go
+++ app/domain/employee.go
@@ -8,9 +8,9 @@ type Employees []Employee
 // A Employee belong to the domain layer.
 type Employee struct {
 	ID         interface{} `json:"id" bson:"_id,omitempty"`
-	FirstName  string      `json:"first_name" bson:"first_name,omitempty"`
-	LastName   string      `json:"last_name" bson:"last_name,omitempty"`
-	PositionID interface{} `json:"position_id" bson:"position,omitempty"`
+	FirstName  string      `json:"first_name" bson:"first_name,omitempty" binding:"required"`
+	LastName   string      `json:"last_name" bson:"last_name,omitempty" binding:"required"`
+	PositionID interface{} `json:"position_id" bson:"position,omitempty" binding:"required"`
 	UpdatedAt  time.Time   `json:"updated_at" bson:"updated_at,omitempty"`
 	CreatedAt  time.Time   `json:"created_at" bson:"created_at,omitempty"`
 }
